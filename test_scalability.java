fbae4d0be385dfd9d8a871934c0bfe6aaadc0f0a
Sivasubramanian.R <sivas@cs.wisc.edu>
2013-12-20 00:38:41 -0600

    read

--- test_scalability.java
+++ test_scalability.java
@@ -72,9 +72,10 @@ public class test_scalability {
 			else if( ja%3 ==1) {
 	    			input = str[(j%2)+1] + random_str[k]+ k+ ";"+random_str[(k+j)%7]+i+j; 
 			}
-			else if( ja%3 ==2) {*/
+			else if( ja%3 ==2) {
 	    			input = str[3] + random_str[k]+ k+ ";"+random_str[(k+j)%7]+i+j+";"+random_str[(i+j)%7]+k; 
-			//}
+			*/}
+			input = str[1] + random_str[k]+ k+ ";"+random_str[(k+j)%7]+i+j;
 	     	        Runnable worker = new WorkerThread_Client(input, s, millis, j);
 	                executor.execute(worker);
     		}
